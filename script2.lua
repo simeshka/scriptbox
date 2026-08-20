6fad89320a25cfb5470794515cb8071e0057c675
simeshka <msmgrind91@gmail.com>
2026-08-20 22:56:50 +0300

    Update script2.lua

--- script2.lua
+++ script2.lua
@@ -1,10 +1,19 @@
+local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
-local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+
+local player = Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
 local root = char:WaitForChild("HumanoidRootPart")
 local humanoid = char:WaitForChild("Humanoid")
+local camera = workspace.CurrentCamera
 
 humanoid.AutoRotate = false
 
+local speed = math.rad(360 * 50)
+
 RunService.RenderStepped:Connect(function(dt)
-	root.CFrame *= CFrame.Angles(0, math.rad(360 * 50) * dt, 0)
+	local rot = CFrame.Angles(0, speed * dt, 0)
+
+	root.CFrame *= rot
+	camera.CFrame *= rot
 end)
